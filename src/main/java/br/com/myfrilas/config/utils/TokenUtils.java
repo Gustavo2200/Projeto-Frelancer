60aa856ec5bee2230a07ade7c05513cc8692a294
Gustavo2200 <gustavoparreiras123pie@gmail.com>
2024-06-05 11:26:15 -0300

    Loguin de Usuarios

--- src/main/java/br/com/myfrilas/config/utils/TokenUtils.java
+++ src/main/java/br/com/myfrilas/config/utils/TokenUtils.java
@@ -27,10 +27,11 @@ public class TokenUtils {
     public TokenJwtResponse getToken(Map<String, Object> dataAuthentication) {
         String token = JWT.create()
                 .withIssuer(issuer)
-                .withSubject(dataAuthentication.get("email").toString())
-                .withClaim("user_id", dataAuthentication.get("usuario_id").toString())
-                .withClaim("email", dataAuthentication.get("email").toString())
-                .withClaim("password", dataAuthentication.get("senha").toString())
+                .withSubject(dataAuthentication.get("DS_EMAIL").toString())
+                .withClaim("user_id", dataAuthentication.get("NR_ID_USUARIO").toString())
+                .withClaim("email", dataAuthentication.get("DS_EMAIL").toString())
+                .withClaim("password", dataAuthentication.get("DS_SENHA").toString())
+                .withClaim("role", dataAuthentication.get("TP_TIPO_USUARIO").toString())
                 .withExpiresAt(new Date(System.currentTimeMillis() + (1000 * expiryTime)))
                 .sign(Algorithm.HMAC256(secretKey));
 
--- src/main/java/br/com/myfrilas/service/AuthService.java
+++ src/main/java/br/com/myfrilas/service/AuthService.java
@@ -1,11 +1,54 @@
 package br.com.myfrilas.service;
 
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.myfrilas.config.utils.TokenUtils;
+import br.com.myfrilas.dao.user.UserDao;
+import br.com.myfrilas.dto.loguin.LoguinDto;
+import br.com.myfrilas.dto.loguin.TokenJwtResponse;
+import br.com.myfrilas.err.exceptions.FreelasException;
 
 @Service
 public class AuthService {
     
     private TokenUtils tokenUtils; 
+    private UserDao userDao;
+    private PasswordEncoder passwordEncoder;
+
+    public AuthService(TokenUtils tokenUtils, UserDao userDao, PasswordEncoder passwordEncoder) {
+        this.tokenUtils = tokenUtils;
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public TokenJwtResponse getToken(LoguinDto loguinDto){
+        var dataAuthentication = getDataAuthentication(loguinDto);
+        return tokenUtils.getToken(dataAuthentication);
+    }
+
+    private Map<String, Object> getDataAuthentication(LoguinDto loguinDto) {
+        Map<String, Object> dataAuthentication = userDao.getUser(loguinDto.getEmail());
+
+        if (dataAuthentication == null) {
+            throw new FreelasException("Login inválido", HttpStatus.UNAUTHORIZED.value());
+        }
+
+        String passwordFromDb = (String) dataAuthentication.get("DS_SENHA");
+        if (passwordFromDb == null) {
+            throw new FreelasException("Senha não encontrada para o usuário", HttpStatus.UNAUTHORIZED.value());
+        }
+
+        checkPassword(loguinDto.getPassword(), passwordFromDb);
+        return dataAuthentication;
+    }
+
+    private void checkPassword(String rawPassword, String encodedPassword) {
+        if (!passwordEncoder.matches(rawPassword, encodedPassword)) {
+            throw new FreelasException("Senha inválida", HttpStatus.UNAUTHORIZED.value());
+        }
+    }
 }
